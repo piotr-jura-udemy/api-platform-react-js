7fa35f0adafbda81af4d1446b1c871c88f7eab79
Piotr Jura <>
2018-09-29 16:03:20 +0200

    Added store, history, router, store passed through provider and 2 components with routes

---
+++ src/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  render() {
+    return (<div>Hello from LoginForm!</div>)
+  }
+}
+
+export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import createHistory from 'history/createBrowserHistory';
+import {Provider} from "react-redux";
+import {ConnectedRouter} from "react-router-redux";
+import {Route, Switch} from "react-router";
+import App from "./components/App";
+import LoginForm from "./components/LoginForm";
+
+const store = createStore(
+  state => state
+);
+const history = createHistory();
 
 ReactDOM.render((
-  <div>Hello world, a made a change!</div>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/login" component={LoginForm}/>
+        <Route path="/" component={App}/>
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
 ), document.getElementById('root'));
