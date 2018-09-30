9b70df2bf862eef3d4733a34763a68de5eb438f2
Piotr Jura <>
2018-09-30 17:57:59 +0200

    BlogPost render with author name

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import timeago from 'timeago.js';
 
 export class BlogPost extends React.Component {
   render() {
@@ -13,8 +14,17 @@ export class BlogPost extends React.Component {
     }
 
     return (
-      <div>
-        {post.title}
+      <div className="card mb-3 mt-3 shadow-sm">
+        <div className="card-body">
+          <h2>{post.title}</h2>
+          <p className="card-text">{post.content}</p>
+          <p className="card-text border-top">
+            <small className="text-muted">
+              {timeago().format(post.published)} by&nbsp;
+              {post.author.name}
+            </small>
+          </p>
+        </div>
       </div>
     )
   }
