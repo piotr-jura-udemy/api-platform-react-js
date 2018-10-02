66f3566b19c6d5471c57e4d32affa64c3c3399ee
Piotr Jura <>
2018-10-02 18:06:59 +0200

    Adding a form reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import blogPostList from "./reducers/blogPostList";
 import blogPost from "./reducers/blogPost";
 import commentList from "./reducers/commentList";
+import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
   blogPostList,
   blogPost,
-  commentList
+  commentList,
+  form: formReducer
 });
