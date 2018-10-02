d02a176b9fc9320a4632aba97fb88650bb143734
Piotr Jura <>
2018-10-02 18:07:52 +0200

    Adding a form reducer #2

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,10 +3,16 @@ import {reduxForm, Field} from 'redux-form';
 import {renderField} from "../form";
 
 class LoginForm extends React.Component {
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const {handleSubmit} = this.props;
+
     return (
       <div className="text-center">
-        <form className="mt-4">
+        <form className="mt-4" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field name="username" label="Username" type="text" component={renderField} />
           <Field name="password" label="Password" type="password" component={renderField} />
           <button type="submit" className="btn btn-primary btn-big btn-block">Log in</button>
