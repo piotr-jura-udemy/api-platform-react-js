bbe8f70d33ea459b2578be96ceb25832109ddab1
Piotr Jura <>
2018-10-02 16:57:51 +0200

    CommentList component implemented

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Message} from "./Message";
+import timeago from 'timeago.js';
 
 export class CommentList extends React.Component {
   render() {
@@ -12,7 +13,21 @@ export class CommentList extends React.Component {
 
     return (
       <div className="card mb-3 mt-3 shadow-sm">
-        Not done yet...
+        {commentList.map(comment => {
+          return (
+            <div className="card-body border-bottom" key={comment.id}>
+              <p className="card-text mb-0">
+                {comment.content}
+              </p>
+              <p className="card-text">
+                <small className="text-muted">
+                  {timeago().format(comment.published)} by&nbsp;
+                  {comment.author.name}
+                </small>
+              </p>
+            </div>
+          );
+        })}
       </div>
     )
   }
