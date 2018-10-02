3249446c1cb688640bdbc07e6ab6a37f25600a67
Piotr Jura <>
2018-10-02 18:44:24 +0200

    UserLoginSuccess action + auth reducer #2

--- src/reducer.js
+++ src/reducer.js
@@ -3,10 +3,12 @@ import blogPostList from "./reducers/blogPostList";
 import blogPost from "./reducers/blogPost";
 import commentList from "./reducers/commentList";
 import {reducer as formReducer} from 'redux-form';
+import auth from "./reducers/auth";
 
 export default combineReducers({
   blogPostList,
   blogPost,
   commentList,
+  auth,
   form: formReducer
 });
