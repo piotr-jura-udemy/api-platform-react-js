c923314e4074d384c88dd9cf02056281a25cae20
Piotr Jura <>
2018-10-29 15:21:54 +0100

    #64 ImageBrowser animations

--- src/components/ImageBrowser.js
+++ src/components/ImageBrowser.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 export class ImageBrowser extends React.Component {
   render() {
@@ -6,18 +7,26 @@ export class ImageBrowser extends React.Component {
 
     return (
       <div className="row mt-4 mb-4">
-        {
-          images.map(image => {
-            return (
-              <div key={image.id} className="col-md-6 col-lg-4">
-                <div className="mt-2 mb-2">
-                  <img src={`http://localhost:8000${image.url}`}
-                       className="img-fluid" />
-                </div>
-              </div>
-            )
-          })
-        }
+        <TransitionGroup component={null}>
+          {
+            images.map(image => {
+              return (
+                <CSSTransition timeout={1000} classNames="fade" key={image.id}>
+                  <div className="col-md-6 col-lg-4">
+                    <div className="mt-2 mb-2">
+                      <img src={`http://localhost:8000${image.url}`}
+                           className="img-fluid"/>
+                    </div>
+                    <div className="mb-2">
+                      <button type="button"
+                              className="btn btn-outline-danger btn-sm">Remove</button>
+                    </div>
+                  </div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
       </div>
     )
   }
